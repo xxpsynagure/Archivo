081ebb4cf1699d89bbd238f952e242550cff44bc
xxpsynagure <pragnya.nagure2002@gmail.com>
2021-12-30 20:50:15 +0530

    Reg & Login Successful, lil validation left

--- SFMS/urls.py
+++ SFMS/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path("StudentLogin/", views.StudentLogin, name="StudentLogin"),
     path("StudentReg/",views.StudentReg, name="StudentReg"),
     #path("404/", views.notfound, name="ERROR404"),
+    path("doLogin",views.doLogin, name="doLogin"),
+    path("doReg",views.doReg,name="doReg"),
+    path("trial",views.trial),
 ]
 
--- SFMS/views.py
+++ SFMS/views.py
@@ -1,11 +1,13 @@
+from django.db.models.fields import EmailField
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.contrib import messages
 from SFMS import models
+from django.db import connections
 
 # Create your views here.
 def index(response):
-    return HttpResponse('<h1>Student File Management System<h1>')
+    return HttpResponse('<h1>Student File Management System</h1>')
 
 def StudentLogin(request):
     return render (request, "StudentLogin.html")
@@ -13,11 +15,48 @@ def StudentLogin(request):
 def StudentReg(request):
     return render(request, "StudentReg.html")
 
-#def notfound(request):
-#    return render(request,'404.html')
-
 def error_404(request,exception):
     return render(request, "404.html")
 
+def doLogin(request):
+    if(request.method!='POST'):
+        return HttpResponse('<h2>Method Not Allowed</h2>')
+    else:
+        user = request.POST.get("your_username")
+        password = request.POST.get("your_pass")
+        p = models.Registration.objects.raw(f"SELECT * FROM Registration WHERE Username = '{user}' AND  Pass = md5('{password}');")
 
+        if(p):
+            return HttpResponse("<h2>Login Successful</h2>")
+        else:
+            messages.info(request,'Username or Password is incorrect!')
+            return render(request,"StudentLogin.html")
 
+def doReg(request):
+    if(request.method!='POST'):
+        return HttpResponse('<h2>Method Not Allowed</h2>')
+    else:
+        user = request.POST.get("username")
+        usn = request.POST.get("usn")
+        email =request.POST.get("email")
+        college =request.POST.get("college")
+        branch =request.POST.get("branch")
+        passw = request.POST.get("pass")
+        re_pass =request.POST.get("re_pass")
+
+        if(passw==re_pass):
+            cursor = connections['default'].cursor()
+            cursor.execute(f"INSERT INTO Registration VALUES('{usn}','{user}','{email}',md5('{passw}'),'{branch}','{college}');")
+            return HttpResponse("<h2>Registration Successful</h2>")
+        else:
+            return render(request,"StudentReg.html")
+
+def trial(request): #trial purpose
+    posts = models.Registration.objects.all()
+    print(posts)
+    print(posts.query)
+
+    p=models.Registration.objects.raw('SELECT * FROM Registration')[0]
+    print(p.username,p.email)
+    
+    return HttpResponse("hello")
