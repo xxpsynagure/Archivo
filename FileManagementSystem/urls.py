732c1de6554904a74f034ff2ce880125828a0f4a
pravin-pk <19cs119.pravin@sjec.ac.in>
2022-03-28 18:15:04 +0530

    ReadMe commit

--- FileManagementSystem/urls.py
+++ FileManagementSystem/urls.py
@@ -14,15 +14,30 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("SFMS.urls")),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# from django.conf import settings
+# if settings.DEBUG == False:
+#     urlpatterns += re_path('',
+#         url(r'^files/(?P<path>.*)$', 'django.views.static.serve', {
+#             'document_root': settings.MEDIA_ROOT,
+#         }),
+#         url(r'^static/(?P<path>.*)$', 'django.views.static.serve', {
+#             'document_root': settings.STATIC_ROOT,
+#         }),
+# )
 
 handler404 = 'SFMS.views.error_404'
 #handler500 = 'SFMS.views.error_500'
--- README.md
+++ README.md
@@ -1,12 +1,9 @@
-# FileManagementSystem
+# STUDENT FILE MANAGEMENT SYSTEM
+## ARCHIVO
 
-* to run a django web server - `python manage.py runserver`
-
-* to make a new web application - `python manage.py startapp 'appName' `
-
-* whenever you create a new applicatio, add it to the settings.py --> INSTALLED_APPS >> `appname.apps.AppnameConfig`
-
-* create virtual environment venv - `pip install virtualenv`  `virtualenv [directory]` 
-* use for this project (I do always)
-* and pip install django and mysqlclient (requirements.txt) in the venv
-* to load all static files do `python manage.py runserver --insecure` with `debug=False` in settings.py
+* clone this repository
+* create a virtual environment using pip or conda
+* `pip install -r requirements.txt` or `conda install -r requirements.txt`
+* make sure to have sql server running
+* make migrations using `python manage.py makemigrations` and `python manage.py migrate`
+* To run the server use `python manage.py runserver`
