76a5510de7ebbd3247ff995a8fe06ff1bda71b70
pravin-pk <19cs119.pravin@sjec.ac.in>
2022-04-04 17:47:47 +0530

    secret key hidden

--- FileManagementSystem/settings.py
+++ FileManagementSystem/settings.py
@@ -20,7 +20,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=0akl+lut^a!zy8%h!s_+ag($sc(1evlwtox%&8@ad4e!5a7im'
+# ------------------SECRET KEY HIDDEN----------------------------------------
+import os
+import dotenv
+
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
+SECRET_KEY = os.environ['SECRET_KEY']
+# ------------------------------------------------------------------------
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
